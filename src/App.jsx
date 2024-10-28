7be22bd5bc4b55175804429ea862c7ba5680cde9
Esaius2058 <wabwirewild@gmail.com>
2024-10-28 13:01:35 +0300

    Add styles in App.css for layout and Cards component presentation.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Cards from './components/cards';
+
+function App() {
+
+  return (
+    <>
+      <h1>Memory Game</h1>
+      <Cards />
+    </>
+  )
+}
+
+export default App
